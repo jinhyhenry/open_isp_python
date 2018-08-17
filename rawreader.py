a587f0ca9096b47dd0ebc61885694496fb6dbeaa
Henry Jin <jinhyhenry@163.com>
2018-08-17 20:01:58 +0800

    【Action】: Convert raw buffer to a normal buffer 【Todo】: Test this function 【Is Feature】: yes

--- rawreader.py
+++ rawreader.py
@@ -10,7 +10,9 @@ class raw_obj:
 		self.height = height
 		self.type = type_mask
 		self.ori_buf_length = int(((width*height)/4)*5)
+		self.normal_buf_length = width*height
 		self.ori_buf = np.zeros(self.ori_buf_length)
+		self.normal_buf = np.zeros(self.normal_buf_length)
 
 		print(self.ori_buf_length)
 
@@ -22,6 +24,22 @@ class raw_obj:
 			self.ori_buf[i] = ord(raw_file.read(1))
 		raw_file.close()
 
+	def convert_to_normal(self):
+		i = 0
+		j = 0
+		while i<self.normal_buf_length:
+			common_byte = int(self.ori_buf[i])
+			self.normal_buf[j] = (int(self.ori_buf[i+4])<<2)+(0x03&common_byte) #P0
+			j += 1
+			self.normal_buf[j] = (int(self.ori_buf[i+3])<<2)+(0x0C&common_byte) #P1
+			j += 1
+			self.normal_buf[j] = (int(self.ori_buf[i+2])<<2)+(0x30&common_byte) #P2
+			j += 1
+			self.normal_buf[j] = (int(self.ori_buf[i+1])<<2)+(0xC0&common_byte) #P3
+			j += 1
+			i=i+5
+		print('convert_to_normal done')
+
 	def obj_test(self,need_dump):
 		print(self.ori_buf)
 
@@ -29,3 +47,4 @@ class raw_obj:
 if __name__ == '__main__':
 	raw0 = raw_obj(4608,3456,0)
 	raw0.obj_test(0)
+	raw0.convert_to_normal()
